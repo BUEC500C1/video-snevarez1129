150ac4495ff60158c622fc9332ba393d3d6b4b9a
Sebastian Nevarez <snevarez@bu.edu>
2020-02-21 21:26:44 -0500

    can now get tweets

--- twitter.py
+++ twitter.py
@@ -9,3 +9,11 @@ class twitter_scraper():
         auth = tweepy.OAuthHandler(config.get('auth', 'consumer_key').strip(), config.get('auth', 'consumer_secret').strip())
         auth.set_access_token(config.get('auth', 'access_token').strip(), config.get('auth', 'access_token_secret').strip())
         self.api = tweepy.API(auth)
+
+    def tweets(self, handle):
+        try:
+            tweet = self.api.user_timeline(screen_name = handle, count = 5, include_rts = True, result_type = "recent", include_entities = True, tweet_mode = 'extended', lang = "en")
+            return tweet
+        except tweepy.error.TweepError as e:
+            print(e)
+            return ""
